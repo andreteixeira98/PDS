02c31c74ee4e3e19a423c2848ae3a549dc1cef33
andreteixeira98 <andreteixeira@alu.ufc.br>
2020-03-16 01:47:51 -0300

    trabalho1 PDS

--- trabalho1/JogoDaForcaEstruturadoSemPOOEmJava/src/br/ufc/quixada/JogoDaForca.java
+++ trabalho1/JogoDaForcaEstruturadoSemPOOEmJava/src/br/ufc/quixada/JogoDaForca.java
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 public class JogoDaForca {
-	//TO DO
-	public static ArrayList<String> obterPalavras() throws IOException {//pegar palavras de um arquivo.
+	public static ArrayList<String> obterPalavras() throws IOException {
 		File file = new File("src/files/timesDeFutebol.txt");
 		FileReader fr = new FileReader(file);
 		ArrayList<String> palavras = new ArrayList<String>();
@@ -16,9 +15,7 @@ public class JogoDaForca {
 		String palavra;
 		do {
 			palavra = br.readLine();
-			if(palavra != null) {
-				palavras.add(palavra);
-			}
+			if(palavra != null) palavras.add(palavra);
 		}while(palavra != null);
 		br.close();
 		fr.close();
@@ -26,9 +23,7 @@ public class JogoDaForca {
 	}
 	public static char[] initResposta(int tam) {
 		char resposta[] = new char[tam];
-		for(int i = 0;i<tam;i++) {
-			resposta[i] = '-';
-		}
+		for(int i = 0;i < tam; i++) resposta[i] = '-';
 		return resposta;
 	}
 	public static boolean[] criarBoneco() {
@@ -39,81 +34,92 @@ public class JogoDaForca {
 		Random sortearIndice = new Random();
 		return palavras.get(sortearIndice.nextInt(10));
 	}
-	//TO DO
 	public static void desenharCenario(boolean boneco[]) {
-		if(boneco[0]) System.out.println("\t 0");
-		if(boneco[1]) System.out.print("\t/");
-		if(boneco[2]) System.out.print("|");
-		if(boneco[3]) System.out.println("\\");
-		if(boneco[4]) System.out.print("\t/ ");
+		System.out.println(" __________");
+		System.out.println("|          |");
+		if(boneco[0]) System.out.println("|          O");
+		if(boneco[1]) {
+			System.out.print("|         /");
+			if(!boneco[2])
+				System.out.println();
+		}
+		if(boneco[2]) {
+			System.out.print("|");
+			if(!boneco[3])
+				System.out.println();
+		}
+		if(boneco[3]) {
+			System.out.println("\\");
+		}
+		if(boneco[4]) {
+			System.out.print("|         / ");
+			if(!boneco[5])
+				System.out.println();
+		}
 		if(boneco[5]) System.out.println("\\");
+		System.out.println("|");
+		System.out.println("|");
 	}
 	public static boolean completouBoneco(boolean boneco[]) {
-		for(int i = 0; i < boneco.length; i++) {
-			if(!boneco[i]){
-				return false;
-			}
-		}
+		for(int i = 0; i < boneco.length; i++)
+			if(!boneco[i]) return false;
 		return true;
 	}
 	public static boolean acertouPalavra(String palavraSorteada,char resposta[]) {
 		for(int i = 0; i < palavraSorteada.length(); i++) {
-			if(palavraSorteada.charAt(i)!=' ' && 
-					palavraSorteada.charAt(i)!=resposta[i]) {
+			if(palavraSorteada.charAt(i)!=' ' && palavraSorteada.charAt(i)!=resposta[i])
 				return false;
-			}
-		}
-		return true;
+		}return true;
 	}
 	public static void main(String[] args) throws IOException {
 		Scanner lerDoTeclado = new Scanner(System.in);
 		boolean boneco[] = criarBoneco();
-		//TO DO
 		ArrayList<String> palavras = obterPalavras();
 		String palavraSorteada = sortearPalavra(palavras);
 		char resposta[] = initResposta(palavraSorteada.length());
+		ArrayList<Character> letrasChutadas = new ArrayList<Character>();
+		char letraChutada;
 		boolean alterouResposta = false;
+		boolean letraJaChutada = false;
 		int contErros = 0;
+		boolean iniciarJogo = false;
 		
-		char letra;
 		System.out.println("Dica: um time de futebol com "+ palavraSorteada.length() + " letras");
 		while(true) {
 			desenharCenario(boneco);
 			System.out.println();
 			System.out.println(resposta);
+			System.out.println("letras Chutadas:"+letrasChutadas.toString());
 			System.out.print("Digite uma letra:");
-			letra = lerDoTeclado.nextLine().charAt(0);//warning
+			letraChutada = lerDoTeclado.nextLine().charAt(0);//warning
 			alterouResposta = false;
-			
+			letraJaChutada = false;
+			if(!letrasChutadas.contains(letraChutada)) {
+				letrasChutadas.add(letraChutada);
+			}else letraJaChutada = true;
 			for(int i = 0; i < palavraSorteada.length(); i++) {
-				if(palavraSorteada.charAt(i) == letra) {
-					resposta[i] = letra;
+				if(palavraSorteada.charAt(i) == letraChutada) {
+					resposta[i] = letraChutada;
 					alterouResposta = true;
 				}
-			}
-			if(!alterouResposta) {
-				System.out.println("errou!!");
+			}if(!alterouResposta && !letraJaChutada) {
 				boneco[contErros++] = true;
 			}
-			//TO DO
 			if(completouBoneco(boneco)) {
 				desenharCenario(boneco);
+				System.out.println();
 				System.out.println(resposta);
 				System.out.println("GAME OVER!");
 				//reiniciarJogo();
 				break;	
-			}
-			if(acertouPalavra(palavraSorteada, resposta)) {
+			}if(acertouPalavra(palavraSorteada, resposta)) {
 				desenharCenario(boneco);
+				System.out.println();
 				System.out.println(resposta);
 				System.out.println("VOCE VENCEU!");
 				//reiniciarJogo();
 				break;
-			}
-			
-		}
-		lerDoTeclado.close();
-
+			}	
+		}lerDoTeclado.close();
 	}
-
 }
